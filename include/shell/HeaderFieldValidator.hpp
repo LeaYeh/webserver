ce192b89cef014d1d0275808317eafaab41a2a67
leske42 <mria.marika.huszr@gmail.com>
2024-11-19 18:45:17 +0100

    feat: Add prototype of new class HeaderFieldValidator

---
+++ include/shell/HeaderFieldValidator.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HeaderFieldValidator.hpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 18:35:36 by mhuszar           #+#    #+#             */
+/*   Updated: 2024/11/19 18:39:03 by mhuszar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class HeaderFieldValidator
+{
+  public:
+
+    HeaderFieldValidator();
+    ~HeaderFieldValidator();
+    HeaderFieldValidator(const HeaderFieldValidator& other);
+    HeaderFieldValidator& operator=(const HeaderFieldValidator& other);
+
+private:
+
+    
+};
---
+++ source/shell/HeaderFieldValidator.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HeaderFieldValidator.cpp                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 18:42:39 by mhuszar           #+#    #+#             */
+/*   Updated: 2024/11/19 18:44:46 by mhuszar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "HeaderFieldValidator.hpp"
+
+HeaderFieldValidator::HeaderFieldValidator()
+{
+    
+}
+HeaderFieldValidator::~HeaderFieldValidator()
+{
+    
+}
+HeaderFieldValidator::HeaderFieldValidator(const HeaderFieldValidator& other)
+{
+    (void)other;
+}
+
+HeaderFieldValidator& HeaderFieldValidator::operator=(const HeaderFieldValidator& other)
+{
+    if (this == &other)
+        return (*this);
+    else
+        return (*this);
+}
+

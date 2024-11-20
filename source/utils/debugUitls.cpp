50305b5d060a8f817d3aa40788e43fd29430668b
LeaYeh <smile2140@gmail.com>
2024-11-20 16:49:28 +0000

    feat: Add utils for debugging

---
+++ source/utils/debugUitls.cpp
@@ -0,0 +1,19 @@
+#include "debugUtils.hpp"
+
+namespace utils
+{
+
+std::string replaceCRLF(std::string str) {
+    const std::string target = "\r\n";
+    const std::string replacement = "␍\n";
+
+    std::string::size_type pos = 0;
+    while ((pos = str.find(target, pos)) != std::string::npos) {
+        str.replace(pos, target.length(), replacement);
+        pos += replacement.length();
+    }
+
+    return (str);
+}
+
+} // namespace utils

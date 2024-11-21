e1b1962eafec4476d93de5c73d7b5004c0d26378
Lea Yeh <lea.yeh.ml@gmail.com>
2024-11-21 21:14:27 +0000

    refactor: Refactor the abstract class to normal

--- source/kernel/TemplateEngine.cpp
+++ source/kernel/TemplateEngine.cpp
@@ -1,24 +1,24 @@
-#include "ATemplateEngine.hpp"
+#include "TemplateEngine.hpp"
 #include <fstream>
 #include <sstream>
 
 namespace webkernel
 {
-    ATemplateEngine::ATemplateEngine(const std::string& template_path)
+    TemplateEngine::TemplateEngine(const std::string& template_path)
     {
         _loadTemplate(template_path);
     }
 
-    ATemplateEngine::~ATemplateEngine()
+    TemplateEngine::~TemplateEngine()
     {
     }
 
-    void ATemplateEngine::setVariable(const std::string& key, const std::string& value)
+    void TemplateEngine::setVariable(const std::string& key, const std::string& value)
     {
         _variable_map[key] = value;
     }
 
-    std::string ATemplateEngine::render() const
+    std::string TemplateEngine::render() const
     {
         std::string rendered_template = _template_content;
         for (std::map<std::string, std::string>::const_iterator it = _variable_map.begin(); it != _variable_map.end(); it++)
@@ -35,7 +35,7 @@ namespace webkernel
         return (rendered_template);
     }
 
-    void ATemplateEngine::_loadTemplate(const std::string& template_path)
+    void TemplateEngine::_loadTemplate(const std::string& template_path)
     {
         _template_path = template_path;
         std::ifstream template_file(template_path.c_str());

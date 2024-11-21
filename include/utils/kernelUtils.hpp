45765d3ea8031f0f955b2d944ef2722dce80fea7
LeaYeh <smile2140@gmail.com>
2024-11-21 16:01:56 +0000

    feat: Add support function for debug the state

--- include/utils/kernelUtils.hpp
+++ include/utils/kernelUtils.hpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <sys/epoll.h>
 #include <sys/socket.h>
+#include <utility>
+#include <vector>
+#include "defines.hpp"
 
 namespace webkernel
 {
@@ -12,5 +15,6 @@ std::string get_object_mtime(const std::string& path);
 std::string format_size(uint64_t size);
 std::string format_time(time_t time);
 std::string get_object_type(const std::string& path);
+std::string explainEventProcessingState(EventProcessingState state);
 
 } // namespace webkernel
--- source/utils/kernelUtils.cpp
+++ source/utils/kernelUtils.cpp
@@ -84,4 +84,24 @@ std::string format_time(time_t time)
     return (std::string(buffer));
 }
 
+std::string explainEventProcessingState(EventProcessingState state)
+{
+    std::string explanation;
+
+    if (state & INITIAL)
+        explanation += "INITIAL ";
+    if (state & PROCESSING)
+        explanation += "PROCESSING ";
+    if (state & COMPELETED)
+        explanation += "COMPELETED ";
+    if (state & ERROR)
+        explanation += "ERROR ";
+    if (state & WRITE_CHUNKED)
+        explanation += "WRITE_CHUNKED ";
+    if (state & WRITE_OTHERS_CHUNKED)
+        explanation += "WRITE_OTHERS_CHUNKED ";
+
+    return (explanation);
+}
+
 } // namespace webkernel

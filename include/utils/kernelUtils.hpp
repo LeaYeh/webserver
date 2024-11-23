41696ab116bb69ab1fac1a6991e530a55d958599
LeaYeh <smile2140@gmail.com>
2024-11-23 15:59:25 +0000

    feat: Add more support functions to get object metadata

--- include/utils/kernelUtils.hpp
+++ include/utils/kernelUtils.hpp
@@ -1,14 +1,16 @@
-#include "Request.hpp"
-#include "defines.hpp"
 #include <stdint.h>
+#include <string>
 #include <sys/epoll.h>
 #include <sys/socket.h>
-#include <utility>
-#include <vector>
 
 namespace webkernel
 {
 
 std::string explainEpollEvent(uint32_t events);
+std::string get_object_size(const std::string& path);
+std::string get_object_mtime(const std::string& path);
+std::string format_size(uint64_t size);
+std::string format_time(time_t time);
+std::string get_object_type(const std::string& path);
 
 } // namespace webkernel

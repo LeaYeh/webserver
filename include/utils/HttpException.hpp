42c13ddbb7a7071462291b6b1eb28be6ff72bcf6
LeaYeh <smile2140@gmail.com>
2024-12-13 15:17:10 +0000

    tmp

--- include/utils/HttpException.hpp
+++ include/utils/HttpException.hpp
@@ -14,18 +14,15 @@ class HttpException : public std::exception
     const char* what() const throw();
     webshell::StatusCode statusCode() const;
     std::string reasonDetail() const;
-    webshell::ContentType contentType() const;
 
   public:
     HttpException(const webshell::StatusCode& statusCode,
-                  const std::string& reasonDetail,
-                  webshell::ContentType content_type = webshell::TEXT_HTML);
+                  const std::string& reasonDetail);
     virtual ~HttpException() throw();
 
-  private:
+  protected:
     webshell::StatusCode _statusCode;
     mutable std::string _reasonDetail;
-    webshell::ContentType _content_type;
 };
 
 } // namespace utils
--- source/shell/ResponseBuilder.cpp
+++ source/shell/ResponseBuilder.cpp
@@ -2,6 +2,7 @@
 #include "TemplateEngine.hpp"
 #include "shellUtils.hpp"
 #include "utils.hpp"
+#include <string>
 
 namespace webshell
 {
@@ -10,36 +11,9 @@ ResponseBuilder::~ResponseBuilder()
 {
 }
 
-Response ResponseBuilder::buildErrorResponse(StatusCode status_code,
-                                             const std::string& message,
-                                             ContentType content_type)
-{
-    static webkernel::TemplateEngine template_engine;
-    Response response;
-
-    response.setStatusCode(status_code);
-    response.clearHeaders();
-    response.setHeader("Content-Type", contentTypeToString(content_type));
-
-    if (content_type == TEXT_HTML)
-    {
-        template_engine.loadTemplate("./www/html/error_page.html");
-        template_engine.setVariable("STATUS_CODE",
-                                    utils::toString(status_code));
-        template_engine.setVariable("ERROR_REASON", message);
-        response.setBody(template_engine.render());
-    }
-    else
-    {
-        response.setBody(message);
-    }
-
-    return (response);
-}
-
-Response ResponseBuilder::buildResponse(
-    StatusCode status_code, const std::map<std::string, std::string>& headers,
-    const std::string& body, bool body_only)
+Response ResponseBuilder::ok(StatusCode status_code,
+                             const std::map<std::string, std::string>& headers,
+                             const std::string& body, bool body_only)
 {
     Response response;
 
@@ -53,10 +27,62 @@ Response ResponseBuilder::buildResponse(
     {
         response.setStatusCode(status_code);
         response.setHeaders(headers);
+        // response.setHeader("Content-Type", _negotiate_content_type(headers));
         response.setBody(body);
     }
 
     return (response);
 }
+
+
+
+// Response ResponseBuilder::buildErrorResponse(StatusCode status_code,
+//                                              const std::string& message,
+//                                              ContentType content_type)
+// {
+//     static webkernel::TemplateEngine template_engine;
+//     Response response;
+
+//     response.setStatusCode(status_code);
+//     response.clearHeaders();
+//     response.setHeader("Content-Type", contentTypeToString(content_type));
+
+//     if (content_type == TEXT_HTML)
+//     {
+//         template_engine.loadTemplate("./www/html/error_page.html");
+//         template_engine.setVariable("STATUS_CODE",
+//                                     utils::toString(status_code));
+//         template_engine.setVariable("ERROR_REASON", message);
+//         response.setBody(template_engine.render());
+//     }
+//     else
+//     {
+//         response.setBody(message);
+//     }
+
+//     return (response);
+// }
+
+// Response ResponseBuilder::buildResponse(
+//     StatusCode status_code, const std::map<std::string, std::string>&
+//     headers, const std::string& body, bool body_only)
+// {
+//     Response response;
+
+//     if (body_only)
+//     {
+//         response.setStatusCode(UNDEFINED);
+//         response.clearHeaders();
+//         response.setBody(body);
+//     }
+//     else
+//     {
+//         response.setStatusCode(status_code);
+//         response.setHeaders(headers);
+//         response.setBody(body);
+//     }
+
+//     return (response);
+// }
 
 } // namespace webshell

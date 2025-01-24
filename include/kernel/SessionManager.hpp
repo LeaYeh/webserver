6d9b9c71e7410cac612d2b5279e38fd057812be2
LeaYeh <smile2140@gmail.com>
2025-01-24 20:59:20 +0000

    feat: Implement session structure and manager logic

---
+++ include/kernel/SessionManager.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "IHandler.hpp"
+#include "Reactor.hpp"
+#include "session_types.hpp"
+#include <set>
+
+namespace webkernel
+{
+class SessionManager : public IHandler
+{
+public:
+    void handle_event(int fd, uint32_t events);
+    int get_fd() const;
+
+public:
+    SessionManager(Reactor* reactor, const SessionConfig& config);
+    SessionManager(const SessionManager& other);
+    SessionManager& operator=(const SessionManager& other);
+    ~SessionManager();
+
+private:
+    Reactor* _reactor;
+    SessionConfig _config;
+    int _server_fd;
+    std::map<std::string /* SID */, SessionData> _sessions;
+    std::set<int /* fd */> _client_fds;
+
+private:
+    bool _is_client_socket(int fd) const;
+    bool _is_listening_socket(int fd) const;
+    void _handle_new_connection();
+    void _handle_session_request(int fd);
+    void _handle_session_set(int fd, const SessionMessage& msg);
+    void _handle_session_get(int fd, const SessionMessage& msg);
+    void _handle_session_delete(int fd, const SessionMessage& msg);
+    void _send_resp(int fd, SessionMessageType type, const std::string& data);
+    void _send_error(int fd, const std::string& msg);
+    void cleanup_expired_sessions();
+
+private:
+    SessionManager();
+};
+
+} // namespace webkernel

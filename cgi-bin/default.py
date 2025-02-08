3809bb489fe34e4f1a376f5bff613c0e474875f4
yenthing <yenguyen1806@gmail.com>
2025-02-08 16:30:43 +0000

    feat: Add cgi script to read data from GET&POST

---
+++ cgi-bin/default.py
@@ -0,0 +1,20 @@
+import cgi
+import os
+
+print("Content-type: text/html\n")
+print()
+
+print("<html><head><title>CGI Response</title></head><body>")
+print("<body><h1>CGI Response</h1>")
+
+form = cgi.FieldStorage()
+
+if os.environ['REQUEST_METHOD'] == 'POST':
+    print("<p>POST request</p>")
+    post_data = form.getvalue('data', 'No data sent')
+    print(f"<p>POST data: {post_data}</p>")
+else:
+    print("<p>GET request</p>")
+    print("<p>GET data: {}</p>".format(form.getvalue('data')))
+
+print("</body></html>")
